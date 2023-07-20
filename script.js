71084c5aa1fb63ff20fb94caebd85e2383577bab
Jeff <doylejeff98@gmail.com>
2023-07-20 02:15:25 -0400

    transition contact box

--- script.js
+++ script.js
@@ -26,6 +26,14 @@ function contact(event) {
 
 }
 
+let isModalOpen = false;
 function toggleModal() {
+    
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+   
+    isModalOpen = true;
     document.body.classList += " modal--open";
 }
