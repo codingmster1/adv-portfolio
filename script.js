3764291dd34d524a5a034a749d47e80b22825354
Jeff <doylejeff98@gmail.com>
2023-07-20 01:22:52 -0400

    test error message and using apis

--- script.js
+++ script.js
@@ -4,20 +4,29 @@
 
 function contact(event) {
     event.preventDefault();
-  //  emailjs 
-  //  .sendForm (
-  //      'service_5ue1zrs',
- //       'template_gnv340c',
- //       event.target,
- //       'uaOfhrHzF6IfYqdsd'
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+     emailjs 
+    .sendForm (
+       'service_5ue1zrs',
+       'template_gnv340c',
+        event.target,
+        'uaOfhrHzF6IfYqdsd'
 
- //   ).then(() => {
- //       console.log('this worked')
- //   })
- const loading = document.querySelector('.modal__overlay--loading');
- const success = document.querySelector('.modal__overlay--success');
+    ).then(() => {
+        
+        loading.classList.remove('modal__overlay--visible');
+        success.classList += " modal__overlay--visible";
+   }).catch(() => {
+    loading.classList.remove('modal__overlay--visible');
+    alert(
+        "The email service is temporarily unavailable. Please contact me directly at doylejeff98@gmail.com"
+    );
+   })
+ 
  loading.classList += " modal__overlay--visible"
  setTimeout(() => {
+    
 console.log('it worked 1')
- }, 500);
+ }, 1000);
 }
