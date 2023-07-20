ddca7ad10021145e44aa523a37954187ab56da68
Jeff <doylejeff98@gmail.com>
2023-07-20 10:13:33 -0400

    dark theme

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-//  
-//  
-//  
+
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+   
+}
 
 function contact(event) {
     event.preventDefault();
@@ -26,7 +38,7 @@ function contact(event) {
 
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     
     if (isModalOpen) {
